9e833bb1f6a785b4513b82685fbcaf3c8f0ac0e1
Agent Unicorn <superslap1999@gmail.com>
2020-07-03 10:30:43 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ExpList from './components/ExpList'
+import AddList from './components/AddExp'
+import NavBar from './components/NavBar'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+    <Router>
+      <NavBar />
+      <div className="App">
+        <Switch>
+          <Route path="/add">
+            <AddList />
+          </Route>
+          <Route path="/">
+            <ExpList />
+          </Route>
+        </Switch>
+      </div>
+  </Router>
+  </div>
   );
 }
 
